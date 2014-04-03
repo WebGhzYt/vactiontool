6d837a07a7a3997f04c3da875bf0dcdca9bbbf95
dilip240 <diliprathore240@gmail.com>
2014-04-03 10:20:34 +0530

    issue of DB fixed

--- db/migrate/20140401122531_create_emp_leave_records.rb
+++ db/migrate/20140401122531_create_emp_leave_records.rb
@@ -4,9 +4,6 @@ class CreateEmpLeaveRecord < ActiveRecord::Migration
       t.integer :user_id
       t.integer :total_leaves
       t.integer :leaves_taken
-      t.string :status
-      t.integer :worked_on_off_days
-
       t.timestamps
     end
   end
