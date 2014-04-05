fcc56fc344ccd3952df32949b43cc6ee4a372888
Ankita14 <ankiagrawal19@gmail.com>
2014-04-05 16:34:50 +0530

    Validation applied for leave_type of PL

--- app/models/leave_record.rb
+++ app/models/leave_record.rb
@@ -1,5 +1,6 @@
 class LeaveRecord < ActiveRecord::Base
 	belongs_to :user
+	has_many :leave_requests
 	#attr_accessible :user_id, :total_leaves, :leaves_taken
 
 	validates :user_id, presence: true, length: { maximum: 50 }
--- app/models/leave_request.rb
+++ app/models/leave_request.rb
@@ -1,7 +1,9 @@
 class LeaveRequest < ActiveRecord::Base
-   
+  	validate :leave_request
 
-    #attr_accessible :applied_date, :leave_type, :reason, :leaves_from, :leaves_to, :status, :status_date
+	belongs_to :user
+	belongs_to :leave_record
+
 
 	validates :applied_date, presence: true, length: { maximum: 50 }
 	validates :leave_type, presence: true, length: { maximum: 50 }
@@ -9,6 +11,34 @@ class LeaveRequest < ActiveRecord::Base
 	validates :leaves_from, presence: true, length: { maximum: 50 }
 	validates :leaves_to, presence: true, length: { maximum: 50 }
 	validates :status, presence: true, length: { maximum: 50 }
-	validates :status_date, presence: true, length: { maximum: 50 }
+	#validates :status_date, presence: true, length: { maximum: 50 }
+
+	def leave_request
+		leave_type = self.leave_type
+		leaves_to = self.leaves_to
+		leaves_from = self.leaves_from
+		applied_date = self.applied_date
+
+		leave_days = (leaves_to.to_date - leaves_from.to_date).to_i + 1 
+		logger.debug leave_days
+
+		leave_applied_gap = (leaves_from.to_date - applied_date.to_date).to_i
+		logger.debug leave_applied_gap
+
+
+		@record = LeaveRecord.find_by(@user_id)
+		remaining_leaves = 24 - @record.leaves_taken
+		logger.debug remaining_leaves
+
+		if leave_type == 'PL' && leave_applied_gap < 14
+			errors.add(:base, "Sorry not granted, as you should inform earlier!")
+			logger.debug "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+			logger.debug leave_type
+			logger.debug leave_days
+		end
+		#if 
+	end
+
 	
 end
+
